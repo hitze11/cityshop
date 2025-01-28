75cdbcca7fffab8ba16b5fcb3c7e2495c2b9a46a
hitze <hitze1126@googlemail.com>
2025-01-28 14:11:41 +0100

    added hover

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,15 +2,19 @@ import React from "react";
 
 function Product({ product }) {
   return (
-    <div className="product-card">
+    <div className="product-card group">
       <img
         src={product.image}
         alt={product.name}
-        className="product-image"
+        className="product-image group-hover:scale-150 group-hover:fixed group-hover:top-1/2 group-hover:left-1/2 group-hover:transform group-hover:-translate-x-1/2 group-hover:-translate-y-1/2"
       />
       <h2 className="text-xl">{product.name}</h2>
       <p className="text-lg">{`$${product.price}`}</p>
-      <p className={`text-sm ${product.available ? 'text-green-600' : 'text-black-600'}`}>
+      <p
+        className={`text-sm ${
+          product.available ? "text-green-600" : "text-black-600"
+        }`}
+      >
         {product.available ? "Available" : "Out of Stock"}
       </p>
     </div>
